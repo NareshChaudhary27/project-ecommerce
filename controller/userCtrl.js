a223ea93d90511681f01413795e5a8f5d203d96f
Naresh Kumar <chaudharynaresh2705@gmail.com>
2025-02-22 17:27:22 +0530

    Added error handling, userController, now we can add users

---
+++ controller/userCtrl.js
@@ -0,0 +1,19 @@
+const User = require('../models/userModel.js');
+const asyncHandler = require('express-async-handler');
+
+
+ const createUser = asyncHandler(async (req, res) => {
+    const email = req.body.email;
+    const findUser = await User.findOne({email: email});
+    if(!findUser){
+        // Create a new user
+        const newUser = await User.create(req.body);
+        res.json(newUser);
+    }
+    else{
+        // User already exists
+        throw new Error('User already exists');
+    }
+ });
+
+ module.exports = {createUser};
--- index.js
+++ index.js
@@ -4,14 +4,22 @@ const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require('./routes/authRoute');
+const bodyParser = require('body-parser');
+const { notFound, errorHandler } = require('./middleware/errorHandler');
 
 dbconnect();
 
-app.use('/', (req,res) => {
-    res.send('Hello World');
-}); 
+// app.use('/', (req,res) => {
+//     res.send('Hello World by 713 and 714');
+// }); 
 
-app.use('api/user', authRouter);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/api/user", authRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
